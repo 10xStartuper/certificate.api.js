14b03618699a54a0482223763f3ef28cf4074684
Mukhammadyusuf Abdurakhimov <icoderx@yandex.com>
2022-05-09 02:34:08 +0300

    v1.0.1 ready

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -4,81 +4,44 @@ import Image from 'next/image'
 
 const Home: NextPage = () => {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+    <div className="flex min-h-screen items-center justify-center bg-gray-200">
       <Head>
-        <title>Create Next App</title>
+        <title>Certificate App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
-
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="rounded-md bg-gray-100 p-3 font-mono text-lg">
-            pages/index.tsx
-          </code>
-        </p>
-
-        <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and its API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className="flex h-24 w-full items-center justify-center border-t">
-        <a
-          className="flex items-center justify-center gap-2"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+      <form
+        action="http://localhost:8080/api/create"
+        method="POST"
+        className="flex w-full flex-col rounded-md bg-gray-100 p-5 md:w-2/4"
+      >
+        <input
+          type="text"
+          name="name"
+          id="name"
+          placeholder="Full name"
+          className="m-5 rounded-xl bg-white p-5 outline-none"
+          required
+        />
+        <select
+          name="field"
+          id="fields"
+          className="m-5 rounded-xl bg-white p-5 outline-none"
+          required
         >
-          Powered by{' '}
-          <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-        </a>
-      </footer>
+          <option selected disabled>
+            Select field of the course
+          </option>
+          <option value="Python Programming">Python Programming</option>
+          <option value="2D Design">2D Design</option>
+          <option value="other">other</option>
+        </select>
+        <input
+          type="submit"
+          placeholder="Full name"
+          className="m-5 cursor-pointer rounded-xl bg-blue-500 p-5 text-white outline-none hover:bg-blue-600 "
+          value="Get Certificate"
+        />
+      </form>
     </div>
   )
 }
--- server/controllers/certificate.controller.js
+++ server/controllers/certificate.controller.js
@@ -3,7 +3,8 @@ const generateCertificate = require('../utils/generateCertificate');
 const createCertificate = async (req, res) => {
   const { name, field } = req.body;
   const certificate = await generateCertificate({ name, field });
-  res.status(201).json(certificate);
+  // res.status(201).json(certificate);
+  res.redirect(certificate.image);
 };
 
 module.exports = { createCertificate };
